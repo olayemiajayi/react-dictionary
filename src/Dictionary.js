00108dba17f73cf8a8d610b9753ffe3fa8934d7b
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-06-20 11:11:21 -0600

    Updated the dictionary page

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [Keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+  //Documentation:https://dictionaryapi.dev/
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${Keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${Keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleWordChange(event) {
