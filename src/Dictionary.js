d1c7b143b22b50301b19eb834cbec6585b062a87
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-06-21 10:35:49 -0600

    Updated the Dictionary page

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
+import Results from "./Results.js";
 import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [Keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
   //Documentation:https://dictionaryapi.dev/
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${Keyword}`);
+
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${Keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -25,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleWordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
