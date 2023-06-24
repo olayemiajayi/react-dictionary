12cb66270dab120c49dbdc641d3d9271c0fa559b
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-06-24 08:41:09 -0600

    updated the main page

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -8,6 +9,9 @@ export default function Meaning(props) {
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>{props.meaning.definitions[0].definition}</p>
       <p>{props.meaning.definitions[0].example}</p>
+      <p>
+        <Synonyms />
+      </p>
     </div>
   );
 }
